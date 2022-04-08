cd82902d51b62a50d392b242cd57733c449f655b
周博东 <bdzhou1999@gmail.com>
2022-04-08 09:32:16 +0800

    feat: Adding template files.

---
+++ src/judge_service_client.py
@@ -0,0 +1,14 @@
+import grpc
+import judge_service_pb2
+import judge_service_pb2_grpc
+
+def run():
+    # 连接 rpc 服务器
+    channel = grpc.insecure_channel('localhost:50051')
+    # 调用 rpc 服务
+    stub = judge_service_pb2_grpc.JudgeServiceStub(channel)
+    response = stub.GetFeature(judge_service_pb2.JudgeServiceReq(id=1001))
+    print("Client received: " + response.result)
+
+if __name__ == '__main__':
+    run()
---
+++ src/judge_service_server.py
@@ -0,0 +1,25 @@
+import grpc
+import judge_service_pb2
+import judge_service_pb2_grpc
+
+import asyncio
+import logging
+
+class JudgeService(judge_service_pb2_grpc.JudgeService):
+    def GetFeature(self, request, context):
+        ...
+        return judge_service_pb2.JudgeServiceRsp(result=request.id)
+
+async def serve() -> None:
+    server = grpc.aio.server()
+    judge_service_pb2_grpc.add_GreeterServicer_to_server(JudgeService(), server)
+    listen_addr = '[::]:50051'
+    server.add_insecure_port(listen_addr)
+    logging.info("Starting server on %s", listen_addr)
+    await server.start()
+    await server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(serve())
